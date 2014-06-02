3610c055c4c89c8ae160cabc2e080f52eb7fc250
goregeorge <aries.george@hotmail.com>
2014-06-02 18:14:26 -0500

    3 Despliegues de listas
    Lista de usuarios, de productos y de consolas

--- src/struct/actions/MAdminAction.java
+++ src/struct/actions/MAdminAction.java
@@ -6,20 +6,57 @@ import model.roles.MUser;
 
 import com.opensymphony.xwork2.ActionSupport;
 
+import dataAccess.DAOs.DAOConsoles;
+import dataAccess.DAOs.DAOProducts;
 import dataAccess.DAOs.DAOUsuarios;
+import dataAccess.Entities.Console;
+import dataAccess.Entities.Product;
 
-public class UserListAction extends ActionSupport{
+public class MAdminAction extends ActionSupport{
 	
 	private List<MUser> users;
-	
-	public List<MUser> getUsers(){
-		return this.users;
-	}
+	private List<Product> products;
+	private List<Console> consoles;
 	
 	public String listUsers() throws Exception{
 		DAOUsuarios daousers = new DAOUsuarios();
-		this.users = daousers.getAllUsers();
-		return SUCCESS;
+		users = daousers.getAllUsers();
+		return "displayUsersList";
+	}
+	
+	public String listProducts() throws Exception{
+		DAOProducts daoproducts = new DAOProducts();
+		products = daoproducts.getAllProducts();
+		return "displayProductsList";
+	}
+	
+	public String listConsoles() throws Exception{
+		DAOConsoles daoconsoles = new DAOConsoles();
+		setConsoles(daoconsoles.getAllConsoles());
+		return "displayConsolesList";
+	}
+	
+	public List<MUser> getUsers(){
+		return users;
+	}
+	
+	public void setUsers(List<MUser> users){
+		this.users = users;
 	}
 
+	public List<Product> getProducts() {
+		return products;
+	}
+
+	public void setProducts(List<Product> products) {
+		this.products = products;
+	}
+
+	public List<Console> getConsoles() {
+		return consoles;
+	}
+
+	public void setConsoles(List<Console> consoles) {
+		this.consoles = consoles;
+	}
 }
